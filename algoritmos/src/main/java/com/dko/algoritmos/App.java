191c2da0d4e5aef06e7943f0445cb5f14151784a
dreyko <dreyko@DESKTOP-8SOR42N>
2017-01-18 22:49:24 -0600

    Ej_01 algoritmos

---
+++ algoritmos/src/main/java/com/dko/algoritmos/App.java
@@ -0,0 +1,13 @@
+package com.dko.algoritmos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
